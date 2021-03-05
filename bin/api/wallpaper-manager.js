831427d8a1fb74156ddfb700abfae09b50e89386
Chance Linz 😎 <chanlinz@amazon.com>
2021-03-05 10:41:26 -0800

    feat(cli): Now respecting DEBUG flag in WallpaperManager class

--- bin/api/wallpaper-manager.js
+++ bin/api/wallpaper-manager.js
@@ -2,8 +2,17 @@ const wallpaper = require("wallpaper");
 
 class WallpaperManager {
   static #isWindows = process.platform === "win32";
+  static #isDebug = process.env.DEBUG;
 
   static async setWallpaper(path) {
+    if (this.#isDebug) {
+      console.log(
+        `\n[DEBUG]: Debug mode is active. Not setting wallpaper. Path: ${path}.`
+      );
+
+      return;
+    }
+
     if (this.#isWindows) {
       await wallpaper.set(path);
     } else {
